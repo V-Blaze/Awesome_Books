b1fec3f1f6581f5d2503bd14613923e8fd07d4a0
V-Blaze <valentine6586@gmail.com>
2022-10-24 14:50:12 +0100

    fix linter errors

--- script.js
+++ script.js
@@ -10,62 +10,59 @@ let formData;
 let bookCollectionHtml;
 
 const addBookToStorage = () => {
-	const str = JSON.stringify(bookCollection);
-	localStorage.setItem('storedBookData', str);
+  const str = JSON.stringify(bookCollection);
+  localStorage.setItem('storedBookData', str);
 };
 
 const deleteBook = (id) => {
-	const itemToDelete = bookCollection[id];
+  const itemToDelete = bookCollection[id];
 
-	bookCollection = bookCollection.filter((item) => item !== itemToDelete);
-	addBookToStorage();
-	showBooks();
+  bookCollection = bookCollection.filter((item) => item !== itemToDelete);
+  addBookToStorage();
+  // eslint-disable-next-line no-use-before-define
+  showBooks();
 };
 
 const addBtnRemoveEvent = () => {
-	document.querySelectorAll('.delete_btn').forEach((button) =>
-		button.addEventListener('click', (event) => {
-			event.preventDefault();
-			const { id } = button;
-			deleteBook(id);
-		})
-	);
+  document.querySelectorAll('.delete_btn').forEach((button) => button.addEventListener('click', (event) => {
+    event.preventDefault();
+    const { id } = button;
+    deleteBook(id);
+  }));
 };
 
 const showBooks = () => {
-	formData = JSON.parse(localStorage.getItem('storedBookData'));
-	bookCollection = formData;
-	allBooks.innerHTML = '';
-	formData.forEach((book, index) => {
-		bookCollectionHtml = document.createElement('div');
-		bookCollectionHtml.innerHTML = `
+  formData = JSON.parse(localStorage.getItem('storedBookData'));
+  bookCollection = formData;
+  allBooks.innerHTML = '';
+  formData.forEach((book, index) => {
+    bookCollectionHtml = document.createElement('div');
+    bookCollectionHtml.innerHTML = `
       <h3 class="book-title">${book.title}</h3>
       <h3 class="book-author">${book.author}</h3>
       <button class="delete_btn" id="${index}">Remove</button>
       <hr>
     `;
-		allBooks.appendChild(bookCollectionHtml);
-	});
+    allBooks.appendChild(bookCollectionHtml);
+  });
 
-	addBtnRemoveEvent();
+  addBtnRemoveEvent();
 };
 
-// eslint-disable-next-line no-use-before-define
-
 addForm.addEventListener('submit', (event) => {
-	event.preventDefault();
-	newBook = {
-		title: addTitle.value,
-		author: addAuthor.value
-	};
-	bookCollection.push(newBook);
-	addBookToStorage();
+  event.preventDefault();
+  newBook = {
+    title: addTitle.value,
+    author: addAuthor.value,
+  };
+  bookCollection.push(newBook);
+  addBookToStorage();
 
-	showBooks();
+  showBooks();
 });
 
 window.onload = () => {
-	if (localStorage.getItem('storedBookData') !== null) {
-		showBooks();
-	}
+  if (localStorage.getItem('storedBookData') !== null) {
+    showBooks();
+  }
 };
