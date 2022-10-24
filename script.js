0b03e2819abdbfde1580c5f2f98d09301ccb0507
mkaya13 <53873573+mkaya13@users.noreply.github.com>
2022-10-24 13:55:49 +0300

    Adds the books into localStorage and shows them on the page

--- script.js
+++ script.js
@@ -1,17 +1,39 @@
 const bookCollection = [];
 
-const addTitle = document.querySelector('#addTitle');
-const addAuthor = document.querySelector('#addAuthor');
-const addForm = document.querySelector('#addBook');
+const addTitle = document.querySelector("#addTitle");
+const addAuthor = document.querySelector("#addAuthor");
+const addForm = document.querySelector("#addBook");
+const allBooks = document.querySelector(".books");
 
 let newBook;
+let formData;
+let bookCollectionHtml;
 
-addForm.addEventListener('submit', (event) => {
-  event.preventDefault(); 
-    newBook = {
-      title: addTitle.value,
-      author: addAuthor.value,
-  }
+addForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  newBook = {
+    title: addTitle.value,
+    author: addAuthor.value,
+  };
   bookCollection.push(newBook);
+  str = JSON.stringify(bookCollection);
+  localStorage.setItem("storedBookData", str);
   console.log(bookCollection);
+
+  showBooks();
 });
+
+let showBooks = () => {
+  formData = JSON.parse(localStorage.getItem("storedBookData"));
+  allBooks.innerHTML = "";
+  formData.forEach((book) => {
+    bookCollectionHtml = document.createElement("div");
+    bookCollectionHtml.innerHTML = `
+      <h3 class="book-title">${book.title}</h3>
+      <h3 class="book-author">${book.author}</h3>
+      <button>Remove</button>
+      <hr>
+    `;
+    allBooks.appendChild(bookCollectionHtml);
+  });
+};
