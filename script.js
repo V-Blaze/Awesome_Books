3779585dc3380c055c2f092dd8d1e10f0a0fc008
mkaya13 <53873573+mkaya13@users.noreply.github.com>
2022-10-25 14:47:28 +0300

    Adds styling to the project

--- script.js
+++ script.js
@@ -9,68 +9,65 @@ let newBook;
 let formData;
 let bookCollectionHtml;
 class Book {
-	constructor(title, author) {
-		this.title = title;
-		this.author = author;
-	}
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+  }
 
-	static addBookToStorage() {
-		const str = JSON.stringify(bookCollection);
-		localStorage.setItem('storedBookData', str);
-	}
+  static addBookToStorage() {
+    const str = JSON.stringify(bookCollection);
+    localStorage.setItem('storedBookData', str);
+  }
 
-	static deleteBook(id) {
-		const itemToDelete = bookCollection[id];
-	
-		bookCollection = bookCollection.filter((item) => item !== itemToDelete);
-	};
+  static deleteBook(id) {
+    const itemToDelete = bookCollection[id];
 
+    bookCollection = bookCollection.filter((item) => item !== itemToDelete);
+  }
 }
 
 const addBtnRemoveEvent = () => {
-	document.querySelectorAll('.delete_btn').forEach((button) =>
-		button.addEventListener('click', (event) => {
-			event.preventDefault();
-			const { id } = button;
-			Book.deleteBook(id);
-			Book.addBookToStorage();
-			// eslint-disable-next-line no-use-before-define
-			showBooks();
-		})
-	);
+  document.querySelectorAll('.delete_btn').forEach((button) => button.addEventListener('click', (event) => {
+    event.preventDefault();
+    const { id } = button;
+    Book.deleteBook(id);
+    Book.addBookToStorage();
+    // eslint-disable-next-line no-use-before-define
+    showBooks();
+  }));
 };
 
 const showBooks = () => {
-	formData = JSON.parse(localStorage.getItem('storedBookData'));
-	bookCollection = formData;
-	allBooks.innerHTML = '';
-	formData.forEach((book, index) => {
-		bookCollectionHtml = document.createElement('div');
-		bookCollectionHtml.innerHTML = `
-      <h3 class="book-title">${book.title}</h3>
-      <h3 class="book-author">${book.author}</h3>
-      <button class="delete_btn" id="${index}">Remove</button>
-      <hr>
-    `;
-		allBooks.appendChild(bookCollectionHtml);
-	});
+  formData = JSON.parse(localStorage.getItem('storedBookData'));
+  bookCollection = formData;
+  allBooks.innerHTML = '';
+  formData.forEach((book, index) => {
+    bookCollectionHtml = document.createElement('div');
+    bookCollectionHtml.className = 'book-item';
+    bookCollectionHtml.innerHTML = `
 
-	addBtnRemoveEvent();
+      <h3 class="book-title"><span>"${book.title}" by ${book.author}</span></h3>
+      <button class="delete_btn" id="${index}">Remove</button>
+    `;
+    allBooks.appendChild(bookCollectionHtml);
+  });
+
+  addBtnRemoveEvent();
 };
 
 addForm.addEventListener('submit', (event) => {
-	event.preventDefault();
-	newBook = new Book(addTitle.value, addAuthor.value);
+  event.preventDefault();
+  newBook = new Book(addTitle.value, addAuthor.value);
 
-	bookCollection.push(newBook);
+  bookCollection.push(newBook);
 
-	Book.addBookToStorage();
+  Book.addBookToStorage();
 
-	showBooks();
+  showBooks();
 });
 
 window.onload = () => {
-	if (localStorage.getItem('storedBookData') !== null) {
-		showBooks();
-	}
+  if (localStorage.getItem('storedBookData') !== null) {
+    showBooks();
+  }
 };
